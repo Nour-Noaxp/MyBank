5b84ee65658b52d9ccd1f0c57f17a1a00bf78185
Nour <nour.ayed@noaxp.com>
2025-03-20 20:01:29 +0100

    Create budget, account, category and transaction models

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Budget(models.Model):
+  name = models.CharField(max_length=50, null=False)
+  ready_to_assign = models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class Account(models.Model):
+  budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+  name = models.CharField(max_length=50, null=False)
+  type = models.TextChoices("Cash Accounts", "Credit Accounts", "Mortgages and Loans", "Tracking Accounts")
+  working_balance = models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+  budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+  name = models.CharField(max_length=50, null=False)
+  activity = models.IntegerField(default=0)
+  available = models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class Transaction(models.Model):
+  account = models.ForeignKey(Account, on_delete=models.CASCADE)
+  category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+  date = models.DateTimeField(null=False)
+  payee = models.CharField(max_length=50)
+  memo = models.CharField(max_length=100)
+  outflow = models.IntegerField(default=0)
+  inflow = models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
