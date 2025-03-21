b9cfbdd42f72e3ef9c0ac34ee6f1740846cd0198
Nour <nour.ayed@noaxp.com>
2025-03-21 17:47:35 +0100

    Remove unnecessary code and db. sqlite3 file

--- budget/views.py
+++ budget/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Account
 from .forms import AccountForm
