997eae3966fa58f8172d0075d5404a968fb95234
Nour <nour.ayed@noaxp.com>
2025-03-26 16:29:11 +0100

    Remove humanize_underscore and is_dict filters

--- budget/templatetags/custom_filters.py
+++ budget/templatetags/custom_filters.py
@@ -7,11 +7,3 @@ def add_class(value, arg):
     if hasattr(value, "as_widget"):
         return value.as_widget(attrs={"class": arg})
     return value
-
-@register.filter(name="humanize_underscore")
-def humanize_underscore(value):
-    return value.replace("_", " ").title()
-
-@register.filter(name="is_dict")
-def is_dict(value):
-    return isinstance(value, dict)
