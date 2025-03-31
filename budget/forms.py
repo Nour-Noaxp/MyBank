317f5ecfff518e1bedac3a57e8575bf87aec2ff9
Nour Ayed <nour.ayed@noaxp.com>
2025-03-31 15:39:15 +0200

    Create AssignForm form and assign partial template

--- budget/forms.py
+++ budget/forms.py
@@ -1,5 +1,6 @@
+from django import forms
 from django.forms import ModelForm
-from .models import Account, Category
+from .models import Budget, Account, Category
 
 
 class AccountForm(ModelForm):
@@ -12,3 +13,24 @@ class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = ("name",)
+
+
+class AssignForm(ModelForm):
+    budget = Budget.objects.first()
+    amount = forms.IntegerField(required=True, label="Assign:")
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.filter(budget=budget), required=True
+    )
+
+    def save(self, commit=True):
+        form = form.save(commit=False)
+        form.category = self.cleaned_data["category"]
+        form.amount = self.cleaned_data["amount"]
+        category = form.category
+        budget = category.budget
+        if commit:
+            category.available += form.amount
+            category.save()
+            budget.ready_to_assign -= form.amount
+            budget.save()
+        return form
