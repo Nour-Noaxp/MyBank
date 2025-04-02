52971ac63769fed4ee99d37d3d2bb725b5d9e908
Blatalzar <bataprod@gmail.com>
2025-04-02 17:21:53 +0200

    Fix js

--- budget/static/budget/js/assign.js
+++ budget/static/budget/js/assign.js
@@ -1,23 +1,11 @@
-const assignBtnModal = document.querySelector(".assign-btn-modal");
-const cancelBtn = document.querySelector(".cancel-btn");
-const assignBtn = document.querySelector(".assign-btn");
+const openModalBtn = document.querySelector(".assign-btn-modal");
 const assignModalContainer = document.querySelector(".assign-modal-container");
+const closeModalBtn = document.querySelector(".cancel-btn");
 
-assignBtnModal.addEventListener("click", function () {
+openModalBtn.addEventListener("click", () => {
   assignModalContainer.classList.remove("hidden");
 });
 
-cancelBtn.addEventListener("click", function () {
+closeModalBtn.addEventListener("click", () => {
   assignModalContainer.classList.add("hidden");
 });
-
-assignBtn.addEventListener("click", function () {
-  assignModalContainer.classList.add("hidden");
-});
-
-// assignModalContainer.addEventListener("click", (event) => {
-//   console.log(event.target);
-//   if (event.target !== assignModalContainer) {
-//     assignModalContainer.classList.add("hidden");
-//   }
-// });
