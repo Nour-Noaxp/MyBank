c71ba72499a1389ba07f66a9e99e29b1695e511e
Nour Ayed <nour.ayed@noaxp.com>
2025-04-02 15:19:28 +0200

    Remove comment, rename js const variables and classes names

--- budget/static/budget/js/assign.js
+++ budget/static/budget/js/assign.js
@@ -1,23 +1,11 @@
-const assignBtnModal = document.querySelector(".assign-btn-modal");
-const cancelBtn = document.querySelector(".cancel-btn");
-const assignBtn = document.querySelector(".assign-btn");
+const openModalBtn = document.querySelector(".open-modal-btn");
+const closeModalBtn = document.querySelector(".close-modal-btn");
 const assignModalContainer = document.querySelector(".assign-modal-container");
 
-assignBtnModal.addEventListener("click", function () {
+openModalBtn.addEventListener("click", function () {
   assignModalContainer.classList.remove("hidden");
 });
 
-cancelBtn.addEventListener("click", function () {
+closeModalBtn.addEventListener("click", function () {
   assignModalContainer.classList.add("hidden");
 });
-
-assignBtn.addEventListener("click", function () {
-  assignModalContainer.classList.add("hidden");
-});
-
-// assignModalContainer.addEventListener("click", (event) => {
-//   console.log(event.target);
-//   if (event.target !== assignModalContainer) {
-//     assignModalContainer.classList.add("hidden");
-//   }
-// });
