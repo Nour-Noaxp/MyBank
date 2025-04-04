8566ad38af8c30f1828a9866226423ec82a9f458
Nour Ayed <nour.ayed@noaxp.com>
2025-04-04 10:46:26 +0200

    quit

--- budget/signals.py
+++ budget/signals.py
@@ -1,11 +1,18 @@
 from .models import Category, Budget
+import django.dispatch
+
+budget_assign_signal = django.dispatch.Signal()
 
 
-def update_category_and_budget(category_id, amount):
-    category = Category.objects.get(id=category_id)
+def update_category_and_budget(sender, **kwargs):
     budget = Budget.objects.first()
+    category = Category.objects.get(id=kwargs.get("category_id"))
+    amount = kwargs.get("amount")
 
-    category.available = category.available + amount
     budget.ready_to_assign = budget.ready_to_assign - amount
-    category.save()
+    category.available = category.available + amount
     budget.save()
+    category.save()
+
+
+budget_assign.connect(update_category_and_budget)
--- budget/views.py
+++ budget/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Account, Budget, Category
 from .forms import AccountForm, CategoryForm
-from .signals import update_category_and_budget
+
+# from .signals import update_category_and_budget
+from .signals import budget_assign_signal
 
 
 def dashboard_view(request):
@@ -16,11 +18,19 @@ def dashboard_view(request):
     )
 
 
+class AssignForm:
+    def __init__(self):
+        pass
+
+
 def budget_assign_view(request):
     if request.method == "POST":
         category_id = request.POST.get("category")
         amount = int(request.POST.get("amount"))
-        update_category_and_budget(category_id, amount)
+        # update_category_and_budget(category_id, amount)
+        budget_assign_signal.send(
+            sender=AssignForm, category_id=category_id, amount=amount
+        )
         messages.success(request, "Budget Successfully Assigned to category")
         return redirect("dashboard")
     messages.error(request, "Invalid data, please the amount and category")
