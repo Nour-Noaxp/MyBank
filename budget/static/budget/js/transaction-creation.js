445992e87fb239f6bd9542ad862328c0875044fb
Nour Ayed <nour.ayed@noaxp.com>
2025-04-07 15:24:44 +0200

    Create transaction layout, and transaction js to show/hide the form

---
+++ budget/static/budget/js/transaction-creation.js
@@ -0,0 +1,11 @@
+const transactionForm = document.querySelector(".transaction-form");
+const addTransactionBtn = document.querySelector(".add-transaction-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const saveBtn = document.querySelector(".saveBtn");
+
+addTransactionBtn.addEventListener("click", () => {
+  transactionForm.classList.remove("hidden");
+});
+cancelBtn.addEventListener("click", () => {
+  transactionForm.classList.add("hidden");
+});
