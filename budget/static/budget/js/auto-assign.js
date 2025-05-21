bfcd6b73ac47f68781864925204b303467418f45
Nour Ayed <nour.ayed@noaxp.com>
2025-05-21 09:34:01 +0200

    Front - Create assign section and modal with js

---
+++ budget/static/budget/js/auto-assign.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JS chargé");
+  const underfundedBloc = document.querySelector(".underfunded-bloc");
+  const assignModal = document.querySelector(".assign-modal");
+  const closeModal = document.querySelector(".close-modal");
+  const assignMoney = document.querySelector(".assign-button");
+
+  underfundedBloc.addEventListener("click", (e) => {
+    console.log("underfunded clicked");
+    assignModal.classList.remove("hidden");
+  });
+
+  closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    assignModal.classList.add("hidden");
+  });
+
+  assignMoney.addEventListener("click", (e) => {
+    e.preventDefault();
+    assignModal.classList.add("hidden");
+  });
+});
