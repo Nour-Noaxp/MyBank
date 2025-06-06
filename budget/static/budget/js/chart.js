7a87b7b0c293262d0badad47cdc8f22a2834120a
Nour Ayed <nour.ayed@noaxp.com>
2025-06-06 08:53:23 +0200

    Reduce Chart graph size, display total spending in the center  and custom chart legend

--- budget/static/budget/js/chart.js
+++ budget/static/budget/js/chart.js
@@ -3,8 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const chartLabels = JSON.parse(chart.dataset.chartLabels);
   const chartData = JSON.parse(chart.dataset.chartData);
   const totalSpending = JSON.parse(chart.dataset.totalSpendingData);
+  const spendingPerCategory = document.querySelector(".spending_per_category");
 
-  new Chart(chart, {
+  const doughnutCenterValue = {
+    id: "doughnutCenter",
+    beforeDatasetsDraw(chart) {
+      const ctx = chart.ctx;
+      ctx.save();
+      const xCoor = chart.getDatasetMeta(0).data[0].x;
+      const yCoor = chart.getDatasetMeta(0).data[0].y;
+      ctx.font = "20px sans-serif";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(`Total Spending`, xCoor, yCoor - 15);
+      ctx.fillText(`${totalSpending}€`, xCoor, yCoor + 15);
+    },
+  };
+
+  const chartGraph = new Chart(chart, {
     type: "doughnut",
     data: {
       labels: chartLabels,
@@ -16,12 +32,34 @@ document.addEventListener("DOMContentLoaded", () => {
         },
       ],
     },
+
     options: {
-      scales: {
-        y: {
-          beginAtZero: true,
+      plugins: {
+        legend: {
+          display: false,
         },
       },
+      aspectRatio: 2,
     },
+    plugins: [doughnutCenterValue],
   });
+
+  const chartLegend = (chart, labels, data) => {
+    const chartColors = chart.data.datasets[0].backgroundColor;
+
+    labels.forEach((label, index) => {
+      const color = chartColors[index];
+      const value = data[index];
+
+      spendingPerCategory.innerHTML += `
+        <div class="flex justify-between items-center mb-1">
+          <div class="flex items-center gap-2">
+            <span class="inline-block rounded-full w-3 h-3" style="background-color:${color};"></span>
+            <span class="categories">${label}</span>
+          </div>
+          <div class="spending">${value}€</div>
+        </div>`;
+    });
+  };
+  chartLegend(chartGraph, chartLabels, chartData);
 });
