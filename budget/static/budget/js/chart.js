642aa4ef7e823760c6df78caa65d6cfa26902155
Nour Ayed <nour.ayed@noaxp.com>
2025-06-16 10:00:35 +0200

    Rename data attribute

--- budget/static/budget/js/chart.js
+++ budget/static/budget/js/chart.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const chartLabels = JSON.parse(chart.dataset.chartLabels);
   const chartData = JSON.parse(chart.dataset.chartData);
   const chartPercentages = JSON.parse(chart.dataset.chartPercentages);
-  const totalSpending = chart.dataset.totalSpendingData;
+  const totalSpending = chart.dataset.totalSpending;
   const spendingPerCategoryContainer = document.querySelector(
     ".spending_per_category_container"
   );
