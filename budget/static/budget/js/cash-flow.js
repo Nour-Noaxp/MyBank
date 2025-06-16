8bfde4104da4ac7e0304f6636454c4bc81a67c51
Nour Ayed <nour.ayed@noaxp.com>
2025-06-16 17:40:53 +0200

    Initialize cash flow chart with random data

---
+++ budget/static/budget/js/cash-flow.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const chart = document.querySelector(".cash-flow-chart");
+  const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const CHART_COLORS = {
+    red: "rgb(255, 99, 132)",
+    blue: "rgb(54, 162, 235)",
+  };
+
+  const labels = MONTHS;
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Income",
+        data: [
+          1200, 1250, 1200, 1380, 1214, 900, 2356, 1300, 1400, 1250, 1500, 1450,
+        ],
+        borderColor: CHART_COLORS.red,
+        backgroundColor: CHART_COLORS.red,
+        borderWidth: 2,
+        borderRadius: 5,
+      },
+      {
+        label: "Spending",
+        data: [20, 1300, 1190, 324, 1214, 356, 2356, 276, 55, 60, 80, 98],
+        borderColor: CHART_COLORS.blue,
+        backgroundColor: CHART_COLORS.blue,
+        borderWidth: 2,
+        borderRadius: 5,
+      },
+    ],
+  };
+
+  const cashFlowGraph = new Chart(chart, {
+    type: "bar",
+    data: data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Income vs. Spending",
+        },
+      },
+    },
+  });
+});
